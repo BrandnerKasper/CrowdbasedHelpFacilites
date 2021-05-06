d528731502fde0208bc7891f4bc74da765a47b4b
Tobias Brandner <tobias.brandner@stud-mail.uni-wuerzburg.de>
2021-05-06 08:55:00 +0200

    code clean up of Tool-Tip PopUp

--- Crowdbased-HFs-Godot/scripts/Control_Tool-Tip_Test.gd
+++ Crowdbased-HFs-Godot/scripts/Control_Tool-Tip_Test.gd
@@ -2,28 +2,39 @@ extends Control
 
 onready var tool_tip = preload("res://scenes/PopUp.tscn") 
 
-#export var title = "Title of Game Object"
+# Editor variables
 export var body_text = "Default Text"
 export var blog_button_url = "http://godotengine.org"
 export var video_button_url = "https://www.youtube.com/watch?v=42HKCFf5Lf4"
+
+# Variables
 var control_width
 var control_height
 var tool_tip_instance
 var show = false
 
+# Initialize tool-tip position and content
 func _ready():
+	_init_tool_tip()
+	_set_tool_tip_variables()
+	
+	
+func _init_tool_tip():
 	control_width = rect_size.x
 	control_height = rect_size.y
 
 	tool_tip_instance = tool_tip.instance()
-	tool_tip_instance.rect_position = get_global_transform_with_canvas().origin - Vector2(-control_width, control_height/2)
+	tool_tip_instance.rect_position = get_global_transform_with_canvas().origin - Vector2(-control_width + 20, control_height + 100)
 	
 	add_child(tool_tip_instance)
-	tool_tip_instance.title = get_parent().get_name()
-	tool_tip_instance.body_text = body_text
-	tool_tip_instance.blog_button_url = blog_button_url
-	tool_tip_instance.video_button_url = video_button_url
 
+func _set_tool_tip_variables():
+	tool_tip_instance._set_Title(get_parent().get_name())
+	tool_tip_instance._set_Body_Text(body_text)
+	tool_tip_instance._set_Blog_URL(blog_button_url)
+	tool_tip_instance._set_Video_URL(video_button_url)
+
+# Handle visibility of tool-tip
 func _process(_delta):
 	
 	if(tool_tip_instance.mouse_hovered or show):
@@ -36,10 +47,9 @@ func _process(_delta):
 		if has_node("PopUp"):
 			$"PopUp".hide()
 
+
 func _on_Control_mouse_entered():
 	show = true
 
-
 func _on_Control_mouse_exited():
 	show = false
-	pass
--- Crowdbased-HFs-Godot/scripts/PopUp.gd
+++ Crowdbased-HFs-Godot/scripts/PopUp.gd
@@ -1,42 +1,36 @@
 extends Popup
-# maybe get parent name as title?
-
-# Editor variables
-export var title = "Title of Game Object"
-export var body_text = "Default Text"
-export var blog_button_url = "http://godotengine.org"
-export var video_button_url = "https://www.youtube.com/watch?v=42HKCFf5Lf4"
 
 # Variables
+var blog_url = "String"
+var video_url = "String"
 var like = 0
 var dislike = 0
 var mouse_hovered = false
 
-#Debug
-var entered = 0
-var exited = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	show()
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	_handle_Title_changed()
-	_handle_Body_Text_changed()
 	_handle_Like_Number_Changed()
 	_handle_DisLike_Number_Changed()
-	#if Engine.editor_hint:
-		#print("Now print while in editor")
+
 
 # Handle Editor Variables
-func _handle_Title_changed():
+func _set_Title(title):
 	$"Nine/Base-M/Base-C/Title-C/Title-Center/Title".text = title
 
-func _handle_Body_Text_changed():
+func _set_Body_Text(body_text):
 	$"Nine/Base-M/Base-C/Body-M/Body-C/Scroll-C/Text_Body".text = body_text
+
+func _set_Blog_URL(url):
+	blog_url = url
+
+func _set_Video_URL(url):
+	video_url = url
 
 func _handle_Like_Number_Changed():
 	$"Nine/Base-M/Base-C/Rating-Center/Rating-C/DisLike-M/DisLike-C/DisLike_Number".text = String(dislike)
@@ -48,42 +42,51 @@ func _handle_DisLike_Number_Changed():
 #Handle Buttons
 func _on_Blog_Button_pressed():
 	print("Blog Button pressed.")
-	var _open_blog = OS.shell_open(blog_button_url)
+	var _open_blog = OS.shell_open(blog_url)
 	if(_open_blog != 0):
 		print("No valid path")
 
 func _on_Video_Button_pressed():
 	print("Video Button pressed.")
-	var _open_video = OS.shell_open(video_button_url)
+	var _open_video = OS.shell_open(video_url)
 	if(_open_video != 0):
 		print("No valid path")
 
 func _on_Like_Button_pressed():
 	print("Like Button pressed.")
 	like += 1
+	_handle_Like_Number_Changed()
 
 func _on_DisLike_Button_pressed():
 	print("DisLike Button pressed.")
 	dislike += 1
+	_handle_DisLike_Number_Changed()
 
 
+# Handle visibility of Tool-Tip PopUp 
 func _on_PopUp_mouse_entered():
 	mouse_hovered = true
 	print("entered: ", mouse_hovered)
 
-#Problem upper Ui elements eat input of mouse hovering, so we have to make clear that we REALLY exited the PopUp
 func _on_PopUp_mouse_exited():
-	var global_mouse_x = get_global_mouse_position().x
-	var global_mouse_y = get_global_mouse_position().y
-	
-	var rectx1 = rect_global_position.x
-	var rectx2 = rect_global_position.x + rect_size.x
-	var recty1 = rect_global_position.y
-	var recty2 = rect_global_position.y + rect_size.y
-	#var Rect01 = Vector2(rectx1, recty1)
-	#var Rect02 = Vector2(rectx2, recty2)
-	
-	#print(get_global_mouse_position(), Rect01, Rect02)
-	if((rectx1 >= global_mouse_x or global_mouse_x >= rectx2) or (recty1 >= global_mouse_y or global_mouse_y >= recty2)):
+	if(_should_PopUp_hide()):
 		mouse_hovered = false
 		print("exited: ", mouse_hovered)
+
+func _should_PopUp_hide():
+	var should_PopUp_hide = false
+	
+	var global_mouse_X = get_global_mouse_position().x
+	var global_mouse_Y = get_global_mouse_position().y
+	
+	var popUp_X_1 = rect_global_position.x
+	var popUp_X_2 = rect_global_position.x + rect_size.x
+	var popUp_Y_1 = rect_global_position.y
+	var popUp_Y_2 = rect_global_position.y + rect_size.y
+
+	# Checks if the mouse is outside the PopUp Rectangle
+	if((popUp_X_1 >= global_mouse_X or global_mouse_X >= popUp_X_2) or (popUp_Y_1 >= global_mouse_Y or global_mouse_Y >= popUp_Y_2)):
+		should_PopUp_hide = true
+		
+	return should_PopUp_hide
+
